117ec03979d3ef7923077841977b70ab5533dd65
cutout <cutout@gmail.com>
2015-08-19 23:27:07 -0400

    combines javascript

---
+++ js/functions.js
@@ -0,0 +1,45 @@
+//Loading state
+$(function(){
+    setTimeout(function(){$("body").addClass("loading")},200),
+    setTimeout(function(){$("body").addClass("loaded")},3000)
+  })
+
+// Tooltips
+$(function() {
+$( document ).tooltip({
+  items: "img, [data-geo], [title]",
+  content: function() {
+    var element = $( this );
+    if ( element.is( "[title]" ) ) {
+      return element.attr( "title" );
+    }
+    if ( element.is( "img" ) ) {
+      return element.attr( "alt" );
+    }
+  }
+});
+});
+
+//Fade on scroll
+$(window).scroll(function(){
+    $(".intro").css("opacity", 1 - $(window).scrollTop() / 880);
+  });
+
+//Smooth scroll to top
+ $(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+//Change position on scroll
+ 
