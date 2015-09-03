1b3c0c9700a4605e0b677b35c27ba0a19cd4bca3
cutout <cutout@gmail.com>
2015-09-02 22:55:15 -0400

    footer slide effect

--- js/functions.js
+++ js/functions.js
@@ -59,3 +59,34 @@ $(window).scroll(function(){
 }
 window.onload = init();
 
+
+
+//Footer fades in
+
+var footer = $('footer'),
+    extra = 10; // In case you want to trigger it a bit sooner than exactly at the bottom.
+
+footer.css({ opacity: '0', display: 'block' });
+
+$(window).scroll(function() {
+
+   var scrolledLength = ( $(window).height() + extra ) + $(window).scrollTop(),
+       documentHeight = $(document).height();
+
+
+    console.log( 'Scroll length: ' + scrolledLength + ' Document height: ' + documentHeight )
+
+   if( scrolledLength >= documentHeight ) {
+
+       footer
+          .addClass('bottom')
+          .stop().animate({ bottom: '0', opacity: '1' }, 200);
+
+   }
+   else if ( scrolledLength <= documentHeight && footer.hasClass('bottom') ) {
+        footer
+           .removeClass('bottom')
+           .stop().animate({ bottom: '20', opacity: '0' }, 500);
+
+   }
+});
